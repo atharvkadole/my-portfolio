ef9e788eb36a499d2c0c2952925d472137ae0195
Atharv Kadole <atharvkadole16@gmail.com>
2025-01-21 22:03:54 +0530

    comments to use and waste code is removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import { useState } from 'react'
 import './App.css'
 import SimpleBottomNavigation from './navigation'
 import Hero from './hero'
 import TechStack from './techstack'
 import ProjectsSection from './prj'
-import Contact from './contact'
-import Fade from '@mui/material/Fade';
-import { useEffect } from 'react';
+import Contact from './contact';
 
 function App() {
-  // const [isLoaded, setIsLoaded] = useState(false);
-  
-  //   useEffect(() => {
-  //     setIsLoaded(true);
-  //   }, []);
-
   return (
     <>
-      {/* <Fade in={isLoaded} timeout={1000}> */}
       <Hero />
       <TechStack />
       <SimpleBottomNavigation />
       <ProjectsSection />
       <Contact />
-      {/* </Fade> */}
     </>
   )
 }
